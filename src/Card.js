4a3623754b9c8511c26e97dd4cc9a5b1873f1b61
Faith Philemon <philemonfaith@gmail.com>
2019-09-25 01:48:57 +0100

    restructure components

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
 
     return (
-        <div className="bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="bg-light-green tc dib br3 pa3 ma2 grow bw3 shadow-5">
             <img src={`https://robohash.org/${id}?size=200x200`}alt="robot" />
             <div>
                 <h2>{name}</h2>
--- src/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Title = () => {
-    return (
-        <h1 className="tc">Hello Hero</h1>
-    );
-}
-
-export default Title;
